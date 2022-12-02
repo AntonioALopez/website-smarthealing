e9bb5641b74e319dd6c2e87da39d0febfe375f21
Antonio Alvarado <antonioalvarado@Antonios-MacBook-Pro.local>
2022-12-02 12:24:30 +0100

    .streamlit added toml

--- smarthealing_app/app.py
+++ smarthealing_app/app.py
@@ -289,4 +289,46 @@ elif tabs == 'Results':
 
     # Plot!
     st.plotly_chart(fig, use_container_width=True)
- 
+    
+# ================================================================
+    df = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+        columns=['lat', 'lon'])
+
+    st.map(df)
+# ================================================================
+
+    import pydeck as pdk
+
+    chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+            'HexagonLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
+    ))
