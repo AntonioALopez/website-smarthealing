32f793db2c96934a7bd866cf25894f1057535719
Antonio Alvarado <antonioalvarado@Antonios-MacBook-Pro.local>
2022-12-05 17:19:27 +0100

    added postal map

--- smarthealing_app/app.py
+++ smarthealing_app/app.py
@@ -75,7 +75,7 @@ if tabs =='Dashboard':
     
     image = Image.open('smarthealing_app/SmartHealing-larg.png')
 
-    st.image(image, caption=None, width=10, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image(image, caption=None, width=250, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     #displaying the image on streamlit app
 
 
